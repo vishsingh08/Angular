73530e7fdfb18ad89d47a179d1e6d18012269f80
vishal Singh Thakur <vish.singh08@gmail.com>
2019-07-20 15:28:28 +0530

    AngularDemo

---
+++ AngularDemo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Marvellous Infosystem’s First Application';
+
+   
+
+}
